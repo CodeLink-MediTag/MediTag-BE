3ca52b7c5858d77ca1f08baf3faa690a415a76da
장경준 <jjangjoon328@naver.com>
2025-02-12 16:54:31 +0900

    feat: JWT 로그인 구현 #11

--- src/main/java/com/example/meditag/domain/member/mapper/MemberMapper.java
+++ src/main/java/com/example/meditag/domain/member/mapper/MemberMapper.java
@@ -1,4 +1,16 @@
 package com.example.meditag.domain.member.mapper;
 
+import com.example.meditag.domain.member.dto.request.RegisterDTO;
+import com.example.meditag.domain.member.entity.Member;
+
 public class MemberMapper {
+
+    // Entity -> DTO
+    public static RegisterDTO toRegisterDTO(Member member) {
+        return RegisterDTO.builder()
+                .username(member.getUsername())
+                .name(member.getName())
+                .phone(member.getPhone())
+                .build();
+    }
 }
