8303e36a7e0cee2bd2aacb809da6189546c737ae
Coseung <dreamdpsh@gmail.com>
2025-02-24 17:35:30 +0900

    feat-mediregist(aws 추가구현 필요) #17
    약 알림 등록, 사진 aws 올리는것은 구현해야함

--- src/main/java/com/example/meditag/domain/medicine/controller/MedicineController.java
+++ src/main/java/com/example/meditag/domain/medicine/controller/MedicineController.java
@@ -1,4 +1,32 @@
 package com.example.meditag.domain.medicine.controller;
 
+import com.example.meditag.domain.medicine.dto.request.MedicineRequestDto;
+import com.example.meditag.domain.medicine.entity.Medicine;
+import com.example.meditag.domain.medicine.repository.MedicineRepository;
+import com.example.meditag.domain.medicine.service.MedicineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/medicines")
+@RequiredArgsConstructor
 public class MedicineController {
+
+    private final MedicineService medicineService;
+    //약 알림 등록
+    @PostMapping("/reminders")
+    public ResponseEntity<String> reminders(@RequestBody MedicineRequestDto requestDto) {
+        medicineService.saveMedicine(requestDto);
+
+        return ResponseEntity.ok("약이 성공적으로 저장되었습니다.");
+    }
+
+
 }
