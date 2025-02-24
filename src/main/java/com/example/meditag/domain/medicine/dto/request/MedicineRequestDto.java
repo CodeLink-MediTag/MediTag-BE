d18e829093d05fe10e76c931dd5ed5a77abf6c82
Coseung <dreamdpsh@gmail.com>
2025-02-24 17:35:30 +0900

    feat-mediregist(aws 추가구현 필요) #17
    약 알림 등록, 사진 aws 올리는것은 구현해야함

--- src/main/java/com/example/meditag/domain/medicine/dto/request/MedicineRequestDto.java
+++ src/main/java/com/example/meditag/domain/medicine/dto/request/MedicineRequestDto.java
@@ -1,4 +1,43 @@
 package com.example.meditag.domain.medicine.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MedicineRequestDto {
+
+    //약 이름
+    @NotBlank(message = "약이름은 필수입니다.")
+    private String name;
+
+    //특징
+    private String characteristic;
+
+    //복용 시작 날짜
+    @NotBlank(message = "복용 시작 날짜는 필수입니다.")
+    private LocalDate startDate;
+
+    //복용 기간
+    @NotBlank(message = "복용기간 입력은 필수입니다.")
+    private int duration;
+
+    //복용 횟수(일반약 일때)
+    @NotBlank(message = "복용횟수 등록은 필수입니다.")
+    private int frequency;
+
+    //사진
+    private String imageUrl;
+
+    //처방약 여부
+    private boolean isPrescribed;
+    //멤버 아이디
+    private Long memberId;
 }
--- src/main/java/com/example/meditag/domain/medicine/service/MedicineService.java
+++ src/main/java/com/example/meditag/domain/medicine/service/MedicineService.java
@@ -1,4 +1,40 @@
 package com.example.meditag.domain.medicine.service;
 
+import com.example.meditag.domain.medicine.dto.request.MedicineRequestDto;
+import com.example.meditag.domain.medicine.entity.Medicine;
+import com.example.meditag.domain.medicine.mapper.MedicineMapper;
+import com.example.meditag.domain.medicine.repository.MedicineRepository;
+import com.example.meditag.domain.member.entity.Member;
+import com.example.meditag.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MedicineService {
+
+    private final MedicineRepository medicineRepository;
+    private final MemberRepository memberRepository;
+
+    public void saveMedicine(MedicineRequestDto requestDto) {
+
+        Member member =memberRepository.findById(requestDto.getMemberId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        Medicine medicine = Medicine.builder()
+                .name(requestDto.getName())
+                .characteristic(requestDto.getCharacteristic())
+                .startDate(requestDto.getStartDate())
+                .duration(requestDto.getDuration())
+                .frequency(requestDto.getFrequency())
+                .imageUrl(requestDto.getImageUrl())
+                .isPrescribed(requestDto.isPrescribed())
+                .member(member)
+                .build();
+
+        medicineRepository.save(medicine);
+
+
+    }
+
 }
