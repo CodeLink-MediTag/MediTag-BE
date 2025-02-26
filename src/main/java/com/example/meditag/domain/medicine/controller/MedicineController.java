1628621f405b4e4ffbbb67a77dbe36bdc76b9d2e
Coseung <dreamdpsh@gmail.com>
2025-02-26 15:37:48 +0900

    update awsS3 upload #22
    aws업로드 presignedUrl 방식

--- src/main/java/com/example/meditag/domain/medicine/controller/MedicineController.java
+++ src/main/java/com/example/meditag/domain/medicine/controller/MedicineController.java
@@ -4,13 +4,11 @@ import com.example.meditag.domain.medicine.dto.request.MedicineRequestDto;
 import com.example.meditag.domain.medicine.entity.Medicine;
 import com.example.meditag.domain.medicine.repository.MedicineRepository;
 import com.example.meditag.domain.medicine.service.MedicineService;
+import com.example.meditag.global.aws.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -20,12 +18,24 @@ import java.util.Map;
 public class MedicineController {
 
     private final MedicineService medicineService;
+    private final S3Service s3Service;
     //약 알림 등록
     @PostMapping("/reminders")
     public ResponseEntity<String> reminders(@RequestBody MedicineRequestDto requestDto) {
+
+
         medicineService.saveMedicine(requestDto);
 
         return ResponseEntity.ok("약이 성공적으로 저장되었습니다.");
+    }
+
+
+
+    @GetMapping("/presigned-url")
+    @ResponseBody
+    String getUrl(@RequestParam String filename){
+        var result = s3Service.createPresignedUrl("test/"+filename);
+        return result;
     }
 
 
