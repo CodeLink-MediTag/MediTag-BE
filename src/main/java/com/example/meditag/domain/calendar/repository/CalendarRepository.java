3905b9655650639b267aeb6f53b1a4f7d40be2b1
장경준 <jjangjoon328@naver.com>
2025-02-28 13:05:51 +0900

    fix: 복약 알림 등록 기능 추가 #24

--- src/main/java/com/example/meditag/domain/calendar/repository/CalendarRepository.java
+++ src/main/java/com/example/meditag/domain/calendar/repository/CalendarRepository.java
@@ -4,8 +4,6 @@ import com.example.meditag.domain.calendar.entity.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 }
