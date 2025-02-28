62f77c43d342839c67d2e5e6c6b99287d0f5eb53
장경준 <jjangjoon328@naver.com>
2025-02-28 16:48:28 +0900

    feat: 캘린더에서 특정 날짜 복용 정보 조회 #31

--- src/main/java/com/example/meditag/domain/calendar/controller/CalendarController.java
+++ src/main/java/com/example/meditag/domain/calendar/controller/CalendarController.java
@@ -1,4 +1,30 @@
 package com.example.meditag.domain.calendar.controller;
 
+import com.example.meditag.domain.auth.dto.CustomUserDetails;
+import com.example.meditag.domain.calendar.dto.response.CalendarGetDateResponseDTO;
+import com.example.meditag.domain.calendar.service.CalendarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/calendar")
+@RequiredArgsConstructor
 public class CalendarController {
+
+    private final CalendarService calendarService;
+
+    // 특정 날짜 복약 정보 조회 API
+    @GetMapping
+    public ResponseEntity<CalendarGetDateResponseDTO> getCalendarByDate(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                                        @RequestParam String date) {
+
+        CalendarGetDateResponseDTO responseDTO = calendarService.getCalendarByDate(customUserDetails.getUsername(), date);
+
+        return ResponseEntity.ok(responseDTO);
+    }
 }
