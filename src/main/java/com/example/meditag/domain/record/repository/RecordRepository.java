5025e81c0bf3b4b5e6cc5c908d8999d054bee3bf
Coseung <dreamdpsh@gmail.com>
2025-03-01 22:30:55 +0900

    저장된 녹음 가져오기 #35
    저장된 녹음 가져오기

--- src/main/java/com/example/meditag/domain/record/repository/RecordRepository.java
+++ src/main/java/com/example/meditag/domain/record/repository/RecordRepository.java
@@ -1,10 +1,14 @@
 package com.example.meditag.domain.record.repository;
 
+import com.example.meditag.domain.member.entity.Member;
 import com.example.meditag.domain.record.entity.Recording;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecordRepository extends JpaRepository<Recording, Long> {
+    List<Recording> findByMemberOrderByRecordingTimeDesc(Member member);
 
 }
