cbeec7dc5ee4e1935bdcde3bd67290b699b30086
장경준 <jjangjoon328@naver.com>
2025-03-07 11:11:46 +0900

    feat: 특정 날짜 복약 정보 조회 #27

--- src/main/java/com/example/meditag/domain/alarm/controller/AlarmController.java
+++ src/main/java/com/example/meditag/domain/alarm/controller/AlarmController.java
@@ -7,16 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/medicines")
 @RequiredArgsConstructor
 public class AlarmController {
 
     private final AlarmService alarmService;
 
     // 복용 여부 API
-    @PatchMapping("/{medicineId}/alarm/{alarmId}/taking")
+    @PatchMapping("/{medicineId}/alarms/{alarmId}/taking")
     public ResponseEntity<String> toggleTaking(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                @PathVariable("medicineId") Long medicineId,
                                                @PathVariable("alarmId") Long alarmId) {
--- src/main/java/com/example/meditag/domain/alarm/mapper/AlarmMapper.java
+++ src/main/java/com/example/meditag/domain/alarm/mapper/AlarmMapper.java
@@ -2,31 +2,8 @@ package com.example.meditag.domain.alarm.mapper;
 
 import com.example.meditag.domain.alarm.dto.response.AlarmResponseDTO;
 import com.example.meditag.domain.alarm.entity.Alarm;
-import com.example.meditag.domain.medicine.entity.Medicine;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class AlarmMapper {
-
-    // 처방약 알림
-    public static Alarm toAlarmsForPrescribedMedicine (Medicine medicine, String dosageTime, LocalDateTime alarmTime) {
-        return Alarm.builder()
-                .medicine(medicine)
-                .dosageTime(dosageTime)
-                .alarmTime(alarmTime)
-                .taking(false)
-                .build();
-    }
-
-    // 일반약 알림
-    public static Alarm toAlarmsForNormalMedicine (Medicine medicine, LocalDateTime alarmTime) {
-        return Alarm.builder()
-                .medicine(medicine)
-                .alarmTime(alarmTime)
-                .taking(false)
-                .build();
-    }
 
     public static AlarmResponseDTO toAlarmResponseDTO(Alarm alarm) {
         return AlarmResponseDTO.builder()
