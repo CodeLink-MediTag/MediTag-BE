74685fb72c64ab4c3498f1c4e51971399e887df8
장경준 <jjangjoon328@naver.com>
2025-05-02 10:09:23 +0900

    chatbot

--- src/main/java/com/example/meditag/domain/calendar/repository/CalendarRepository.java
+++ src/main/java/com/example/meditag/domain/calendar/repository/CalendarRepository.java
@@ -1,6 +1,7 @@
 package com.example.meditag.domain.calendar.repository;
 
 import com.example.meditag.domain.calendar.entity.Calendar;
+import com.example.meditag.domain.medicine.entity.Medicine;
 import com.example.meditag.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
@@ -16,4 +18,6 @@ public interface CalendarRepository extends JpaRepository<Calendar, Long> {
     List<Calendar> findByMemberAndDate(@Param("member") Member member, @Param("date") LocalDate date);
 
     List<Calendar> findByMedicine_Member_Username(String username);
+
+    Optional<Calendar> findByMedicineAndDate(Medicine medicine, LocalDate date);
 }
